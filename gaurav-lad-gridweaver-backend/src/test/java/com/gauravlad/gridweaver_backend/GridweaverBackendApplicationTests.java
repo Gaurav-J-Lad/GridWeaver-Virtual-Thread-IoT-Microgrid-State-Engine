c39db289cda8796220bab6814d2a763d7c78655f
Gaurav Lad <gauravlad1974@gmail.com>
2026-07-16 11:15:05 +0530

    01: Initial Backend Project Setup - Backend with PostgreSQL configuration, dependencies, and package structure

---
+++ gaurav-lad-gridweaver-backend/src/test/java/com/gauravlad/gridweaver_backend/GridweaverBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gauravlad.gridweaver_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GridweaverBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
