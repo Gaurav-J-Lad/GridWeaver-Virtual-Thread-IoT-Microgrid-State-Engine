afaf454601a2531e9dc6e8efc0c69017d43c4f93
Gaurav Lad <gauravlad1974@gmail.com>
2026-07-22 16:02:57 +0530

    07: Created Configuration Classes

---
+++ gaurav-lad-gridweaver-backend/src/main/java/com/gauravlad/gridweaver_backend/config/AppConfig.java
@@ -0,0 +1,8 @@
+package com.gauravlad.gridweaver_backend.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+}
---
+++ gaurav-lad-gridweaver-backend/src/main/java/com/gauravlad/gridweaver_backend/config/WebSocketConfig.java
@@ -0,0 +1,7 @@
+package com.gauravlad.gridweaver_backend.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebSocketConfig {
+}
