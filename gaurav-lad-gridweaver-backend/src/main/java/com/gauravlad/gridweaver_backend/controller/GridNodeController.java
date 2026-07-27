a8799fc7d6736fc87934706c8d77c1f2543590dc
Gaurav Lad <gauravlad1974@gmail.com>
2026-07-27 14:46:08 +0530

    12: Updated GridNodeController Class with APIs and added Validations to GridNodeRequest

--- gaurav-lad-gridweaver-backend/src/main/java/com/gauravlad/gridweaver_backend/controller/GridNodeController.java
+++ gaurav-lad-gridweaver-backend/src/main/java/com/gauravlad/gridweaver_backend/controller/GridNodeController.java
@@ -1,10 +1,54 @@
 package com.gauravlad.gridweaver_backend.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.gauravlad.gridweaver_backend.dto.request.GridNodeRequest;
+import com.gauravlad.gridweaver_backend.dto.response.GridNodeResponse;
+import com.gauravlad.gridweaver_backend.service.GridNodeService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/grid-nodes")
+@RequiredArgsConstructor
 public class GridNodeController {
 
+    private final GridNodeService gridNodeService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public GridNodeResponse createGridNode(
+            @Valid @RequestBody GridNodeRequest request) {
+
+        return gridNodeService.createGridNode(request);
+    }
+
+    @GetMapping
+    public List<GridNodeResponse> getAllGridNodes() {
+
+        return gridNodeService.getAllGridNodes();
+    }
+
+    @GetMapping("/{id}")
+    public GridNodeResponse getGridNodeById(@PathVariable Long id) {
+
+        return gridNodeService.getGridNodeById(id);
+    }
+
+    @PutMapping("/{id}")
+    public GridNodeResponse updateGridNode(
+            @PathVariable Long id,
+            @Valid @RequestBody GridNodeRequest request) {
+
+        return gridNodeService.updateGridNode(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteGridNode(@PathVariable Long id) {
+
+        gridNodeService.deleteGridNode(id);
+    }
 }
--- gaurav-lad-gridweaver-backend/src/main/java/com/gauravlad/gridweaver_backend/dto/request/GridNodeRequest.java
+++ gaurav-lad-gridweaver-backend/src/main/java/com/gauravlad/gridweaver_backend/dto/request/GridNodeRequest.java
@@ -1,12 +1,24 @@
 package com.gauravlad.gridweaver_backend.dto.request;
 
 import com.gauravlad.gridweaver_backend.enums.NodeType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record GridNodeRequest(
+
+        @NotBlank(message = "Node ID is required")
         String nodeId,
+
+        @NotNull(message = "Node Type is required")
         NodeType nodeType,
+
+        @NotNull(message = "Latitude is required")
         Double latitude,
+
+        @NotNull(message = "Longitude is required")
         Double longitude,
+
+        @NotNull(message = "Current Power is required")
         Double currentPower
 ) {
 }
