ab419a8c35e60ddb6c3573711307f8be247c3088
Gaurav Lad <gauravlad1974@gmail.com>
2026-07-29 19:30:12 +0530

    14: Updated Telemetry Request DTO and Battery Request DTO

--- gaurav-lad-gridweaver-backend/src/main/java/com/gauravlad/gridweaver_backend/dto/request/BatteryRequest.java
+++ gaurav-lad-gridweaver-backend/src/main/java/com/gauravlad/gridweaver_backend/dto/request/BatteryRequest.java
@@ -1,9 +1,18 @@
 package com.gauravlad.gridweaver_backend.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+
 public record BatteryRequest(
+        @NotNull
         Long gridNodeId,
+
+        @NotNull
         Double chargeLevel,
+
+        @NotNull
         Double capacity,
+
+        @NotNull
         Double temperature
 ) {
 }
--- gaurav-lad-gridweaver-backend/src/main/java/com/gauravlad/gridweaver_backend/dto/request/TelemetryRequest.java
+++ gaurav-lad-gridweaver-backend/src/main/java/com/gauravlad/gridweaver_backend/dto/request/TelemetryRequest.java
@@ -1,9 +1,21 @@
 package com.gauravlad.gridweaver_backend.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+
 public record TelemetryRequest(
+        @NotNull(message = "Grid Node Id is required")
         Long gridNodeId,
+
+        @NotNull(message = "Power Output is required")
         Double powerOutput,
+
+        @NotNull(message = "Voltage is required")
         Double voltage,
+
+        @NotNull(message = "Current is required")
         Double current,
+
+        @NotNull(message = "Temperature is required")
         Double temperature
 ) {
 }
